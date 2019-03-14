3812ae9d4644671545e91133fdd08345ff5c0999
CM Student <cmstudent@uhwo-acm-13.local>
2019-03-14 10:44:34 -1000

    Finished Parts 3 & 4

--- HackwichFive/ViewController.swift
+++ HackwichFive/ViewController.swift
@@ -10,26 +10,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    
-    @IBOutlet weak var slider: UISlider!
+    @IBOutlet var targetLabel: UILabel!
+
+   @IBOutlet weak var slider: UISlider!
     var currentValue: Int = 0
+    var targetValue: Int = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        currentValue = lroundf(slider.value)
+        targetValue = 1 + Int(arc4random_uniform(100))
+        let thumbImageNormal = UIImage(named: "SliderThumb-Normal")
+        slider.setThumbImage(thumbImageNormal, for: .normal)
     }
 
     @IBAction func sliderHasMoved(_ sender: Any) {
         print("The value of the slider is:\(slider.value)")
         currentValue = lroundf(slider.value)
     }
+    func startNewRound() {
+        targetValue = 1 + Int(arc4random_uniform(100))
+        currentValue = 50
+        slider.value = Float(currentValue)
+        updateTargetLabel()
+        
+        
+    }
+    
+    
     @IBAction func myGuessButtonPressed(_ sender: Any) {
-        let message = "The value is \(currentValue)"
+        let message = "The value is \(currentValue)" + "\nThe target value is: \(targetValue)"
         let alert = UIAlertController(title:"Hello World!", message: message, preferredStyle: .alert)
         let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
+        startNewRound()
     }
-    
+    func updateTargetLabel(){
+        targetLabel.text = String(targetValue)
+        
+    }
     
 }
 
